5fdc049027abd41c4ed262bb7f076e375fe0103d
silaswes <silas.wessely@gmx.de>
2020-11-02 12:39:51 +0100

    ExitFragment Design

--- app/src/main/java/com/example/musicparty/ExitConnectionFragment.java
+++ app/src/main/java/com/example/musicparty/ExitConnectionFragment.java
@@ -1,18 +1,25 @@
 package com.example.musicparty;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ExitConnectionFragment extends Fragment {
 
     public ConfirmExit confirmExit;
+    private TextView partyNameTextView;
+    private static final String NAME = ExitConnectionFragment.class.getName();
 
     public interface ConfirmExit {
         void denyExit();
@@ -35,7 +42,9 @@ public class ExitConnectionFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_exit_connection, container, false);
         //binding = ActivityPartyBinding.inflate(getLayoutInflater());
 
-        Button denyButton = view.findViewById(R.id.denyLeaveParty);
+        partyNameTextView = view.findViewById(R.id.leavePartyNameTextView);
+
+        Button denyButton = view.findViewById(R.id.denyLeavePartyButton);
         denyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,5 +53,17 @@ public class ExitConnectionFragment extends Fragment {
         });
 
         return view;
+    }
+
+    public void setPartyName(String name) {
+        //TODO: Format String partyName
+        if(partyNameTextView != null) {
+            Log.d(NAME, name);
+            partyNameTextView.setText(name, TextView.BufferType.SPANNABLE);
+            Spannable spannable = (Spannable)partyNameTextView.getText();
+            int start = 0;
+            int end = name.length();
+            spannable.setSpan(new StyleSpan(Typeface.BOLD), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
     }
 }
